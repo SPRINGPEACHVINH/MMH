77a588c251177534936961c69fbe6b5b7b9de2a5
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-06-03 02:39:54 +0700

    setting cái khung

---
+++ src/components/Footer/ShowFooter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShowFooter = () => {
+    return (
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    );
+}
+
+export default ShowFooter;
---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import MainMenu from "../pages/MainMeu/MainMenu.jsx";
+
+export const routes = [
+  {
+    path: "/",
+    page: MainMenu,
+    isShowHeader: true,
+    isShowFooter: true,
+  },
+];
