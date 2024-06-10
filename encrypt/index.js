bddf8adc77b657d969897a9f61c4403e8f6f5a43
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-06-10 17:16:32 +0700

    setting in enc

--- encrypt/index.js
+++ encrypt/index.js
@@ -4,14 +4,7 @@ const app = express();
 const port = 5000; // Backend runs on port 5000
 
 app.get('/run-exe', (req, res) => {
-    const exePath = 'path_to_your_exe_file.exe';
-    execFile(exePath, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing file: ${error}`);
-            return res.status(500).send(`Error: ${error.message}`);
-        }
-        res.send(`Output: ${stdout}`);
-    });
+    execFile("./encForWeb", (error, stdout, stderr) => console.log(stdout));
 });
 
 app.listen(port, () => {
